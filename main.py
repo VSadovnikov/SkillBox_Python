42fd91a74a5e657bdd04bcf766d1988ea1ab2f06
Vasiliy Sadovnikov <basilsad1989@gmail.com>
2021-03-29 16:24:47 +0300

    classes

---
+++ main.py
@@ -0,0 +1,12 @@
+class Worker:
+    def __init__(self, name, pay):
+        self.name = name
+        self.pay = pay
+    def lastName(self):
+        return self.name.split()[-1]
+    def giveRaise(self, percent):
+        self.pay *= (1.0 + percent)
+
+
+vasiliy = Worker('Vasiliy Sadovnikov', 20000)
+print(vasiliy.lastName())
