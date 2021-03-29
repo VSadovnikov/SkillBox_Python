8159a45f97e80f8b93dba6367528c6bcdd73932b
Vasiliy Sadovnikov <basilsad1989@gmail.com>
2021-03-29 08:48:23 +0300

    dicts chapter

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+D = {'food': 'Spam', 'guantity' : 4, 'color': 'pink'}
+print(D['food'])
 
+D = {}
+D['Name'] = 'Vasiliy'
+D['job'] = 'dev'
+D['age'] = 31
+
+print(D)
+
+rec = {'name': {'first': 'Bob', 'last': 'Smith'}, 'job': ['dev', 'mgr'], 'age': 31.5}
+rec['job'].append('tm')
+print(rec)
+
+
+D = {'a': 1, 'b': 2, 'c': 3}
+
+Ks = list(D.keys())
 
 
 
